8a6a0a01996f5845c5bb571594f9fcbb0b021545
maltezelkin <maltezescobarvictormanuel@gmail.com>
2021-06-06 13:28:20 -0600

    Calculadora Basica

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import VentanaPrincipal from './Detalle_Calculadora/VentanaPrincipal'
+AppRegistry.registerComponent(appName, () => VentanaPrincipal);
